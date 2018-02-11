f2a3820fae7d1b5116ed1a635ff29201be726e2f
drake <Peter35128@gmail.com>
2018-02-11 13:34:57 +0200

    first commit

---
+++ cast2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace cast2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("dfds");
+        }
+        
+        private static 
+    }
+}
