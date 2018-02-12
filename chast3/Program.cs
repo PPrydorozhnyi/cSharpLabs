eeef302abdd184a85856843332695838bcec8474
drake <Peter35128@gmail.com>
2018-02-12 16:48:11 +0200

    stack tests

--- chast3/Program.cs
+++ chast3/Program.cs
@@ -8,15 +8,19 @@ namespace chast3
     {
         public static void Main(string[] args)
         {
-            TestArrayList();
+//            TestArrayList();
+//            TestSortedList();
+            TestStack();
         }
 
         private static void TestArrayList()
         {
             ArrayList arrayList = new ArrayList();
-            arrayList.Add("Hello");
-            arrayList.Add("World");
-            arrayList.Add("!");
+            
+            arrayList.Add("How");
+            arrayList.Add("are");
+            arrayList.Add("you");
+            arrayList.Add("?");
 
             foreach (IEnumerable instance in arrayList)
             {
@@ -26,7 +30,37 @@ namespace chast3
 
         private static void TestSortedList()
         {
+            SortedList sortedList = new SortedList
+            {
+                {"de eerste", "How"},
+                {"de tweede", "are"},
+                {"de derde", "you"},
+                {"de vierde", "?"}
+            };
+
+            Console.WriteLine("by key\tby index");
             
+            Console.WriteLine("\t{0}\t{1}", sortedList.GetByIndex(sortedList.IndexOfKey("de eerste")), sortedList.GetByIndex(0));
+            Console.WriteLine("\t{0}\t{1}", sortedList.GetByIndex(sortedList.IndexOfKey("de tweede")), sortedList.GetByIndex(1));
+            Console.WriteLine("\t{0}\t{1}", sortedList.GetByIndex(sortedList.IndexOfKey("de derde")), sortedList.GetByIndex(2));
+            Console.WriteLine("\t{0}\t{1}", sortedList.GetByIndex(sortedList.IndexOfKey("de vierde")), sortedList.GetByIndex(3));
+            
+        }
+
+        private static void TestStack()
+        {
+            Stack stack = new Stack();
+            
+            stack.Push("Hi");
+            stack.Push("there");
+            stack.Push("!");
+
+            foreach (IEnumerable instance in stack)
+            {
+                Console.WriteLine(instance);
+            }
+            
+            Console.WriteLine("peek: {0}", stack.Peek());
         }
     }
 }
